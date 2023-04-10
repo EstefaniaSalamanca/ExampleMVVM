618e2afbe4ae1eb98befcc7e83558902e7bfd851
Estefanía <estefaniasalamanca@hotmail.com>
2023-04-10 15:47:25 +0200

    MVVM parte 1

---
+++ ExampleMVVM/app/src/main/java/com/estefaniasalamanca/examplemvvm/model/QuoteModel.kt
@@ -0,0 +1,3 @@
+package com.estefaniasalamanca.examplemvvm.model
+
+data class QuoteModel(val quote:String, val author:String)
---
+++ ExampleMVVM/app/src/main/java/com/estefaniasalamanca/examplemvvm/viewmodel/QuoteViewModel.kt
@@ -0,0 +1,14 @@
+package com.estefaniasalamanca.examplemvvm.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.estefaniasalamanca.examplemvvm.model.QuoteModel
+import com.estefaniasalamanca.examplemvvm.model.QuoteProvider
+
+class QuoteViewModel : ViewModel() {
+    val quoteModel = MutableLiveData<QuoteModel>()
+    fun randomQuote(){
+        val currentQuote:QuoteModel = QuoteProvider.random()
+        quoteModel.postValue(currentQuote)
+    }
+}
